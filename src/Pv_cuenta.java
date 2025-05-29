01281011de095026cfafe393eafb8bbb37ce3202
laboratorio <pedro.vargas@udla.edu.ec>
2025-05-29 16:46:56 -0500

    segundo commit

--- src/Pv_cuenta.java
+++ src/Pv_cuenta.java
@@ -1,2 +1,44 @@
 public class Pv_cuenta {
+    private float saldo;
+    private int numeroDepositos;
+    private int numeroRetiros;
+    private float tasaAnual;
+
+    public Pv_cuenta(float saldo, float tasaAnual) {
+        this.saldo = saldo;
+        this.tasaAnual = tasaAnual;
+        this.numeroDepositos = 0;
+        this.numeroRetiros = 0;
+    }
+
+    public void depositar(float cantidad) {
+        if (cantidad > 0) {
+            saldo += cantidad;
+            numeroDepositos++;
+        }
+    }
+
+    public void retirar(float cantidad) {
+        if (cantidad > 0 && cantidad <= saldo) {
+            saldo -= cantidad;
+            numeroRetiros++;
+        }
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public int getNumeroDepositos() {
+        return numeroDepositos;
+    }
+
+    public int getNumeroRetiros() {
+        return numeroRetiros;
+    }
+
+    public float getTasaAnual() {
+        return tasaAnual;
+    }
 }
+
